9bb04e2bd3eb43d19f4248e416afe3c030bf0549
robinsandday <45364158+robinsandday@users.noreply.github.com>
2020-12-12 08:47:27 +0100

    Update christest.js

--- knackjs/christest.js
+++ knackjs/christest.js
@@ -386,9 +386,9 @@ takePhotoButton.onclick = takePhoto;
       	var c = document.getElementById('cameraAppleCanvas');
  		c.width = video.videoWidth;
 		c.height = video.videoHeight;
-	    	var glContextAttributes = { preserveDrawingBuffer: true }; 
-	    	var gl = canvas.getContext("experimental-webgl", glContextAttributes);
-		//c.getContext('2d').drawImage(video, 0, 0);
+	    	var glContextAttributes = { preserveDrawingBuffer: true };
+	    	var gl = c.getContext("experimental-webgl", glContextAttributes);
+		//c.getContext('2d')
 	    	gl.drawImage(video, 0, 0);
 		// Other browsers will fall back to image/png
 		img.style.visibility = 'visible';
