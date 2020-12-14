fb6cfaa14992386917710bb17263d2d4114dbeb8
HynekHruska <hynek@hruska.cz>
2020-12-14 19:24:05 +0100

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -907,7 +907,7 @@ const constraints = {
  const image = new Image('naturalWidth', 'naturalHeight');
  image.onload = drawImageActualSize;
  //image.src = 'https://raw.githubusercontent.com/robinsandday/Camera_App-for-Image-Overlay/main/car-removebg.png?token=AK2DHPRJXE5E2DFU5EXYCXS7Y6ROW';
- image.src = 'https://raw.githubusercontent.com/robinsandday/Camera_App-for-Image-Overlay/main/car-removebg_v2.png';
+ image.src = 'https://raw.githubusercontent.com/robinsandday/Camera_App-for-Image-Overlay/main/car-removebg_v2o.png';
 
    //this image gets the captured photo and when it is loaded it resizes iteslf and saves the image to shown image
 var imageBeforeResize = document.createElement('img');
