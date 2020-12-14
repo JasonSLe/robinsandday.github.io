8ee0774cdbc6710b8d31bdf1dd5fcbb5c2893226
HynekHruska <hynek@hruska.cz>
2020-12-14 13:15:31 +0100

    dev

--- knackjs/christest.js
+++ knackjs/christest.js
@@ -582,7 +582,7 @@ function uploadImages(infoText){
         continue;
       };
       $('#'+infoText).text('Uploading image');
-      uploadImage(token, updatingRecordId, imagesToUpload.app, $('#'+imagesToUpload.images[i].name).attr('data-fullImageSrc'), imagesToUpload.images[i], infoText).then(function(resp){
+      uploadImage(token, updatingRecordId, imagesToUpload.app, $('#'+imagesToUpload.images[i].name).attr('src'), imagesToUpload.images[i], infoText).then(function(resp){
         alert('now');
       });
       alert('aaa');
