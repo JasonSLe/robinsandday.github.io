80ad903a9864c6ef6b1be78a8c4bd49a83f09980
HynekHruska <hynek@hruska.cz>
2020-12-19 12:25:23 +0100

    dev

--- knackjs/christest.js
+++ knackjs/christest.js
@@ -182,11 +182,11 @@ var OperatingSystem = {
 //************************************* OPEN THE CAMERA BY ASKING USER PERMISSION(APPLE DEVICE) AND APPLY VIDEO STREAM SETTINGS*****************************************
 
 const constraints = {
- width: { min: 1440, ideal: 1280, max: 3984 },
- height: { min: 1080, ideal: 720, max: 2988 },
- aspectRatio: 4/3,
- frameRate:{max: 30},
- zoom:2.0
+    width: { min: 1440, ideal: 1280, max: 3984 },
+    height: { min: 1080, ideal: 720, max: 2988 },
+    aspectRatio: 4/3,
+    frameRate:{max: 30},
+    advanced: [{zoom:2.0}]
   };
 
   navigator.mediaDevices.getUserMedia({video: {pan: true, facingMode: {exact: "environment"}}
