3b30059f55ffdb09f122ff0753af92f3758b2863
HynekHruska <hynek@hruska.cz>
2020-12-19 14:09:57 +0100

    dev

--- knackjs/christest.js
+++ knackjs/christest.js
@@ -273,7 +273,7 @@ imageBeforeResize.onload = () => {
    }
  }
 
- circle.style.display = 'inline';
+ //circle.style.display = 'inline';
 //**************************** SPIRIT LEVEL *****************************************
  function handleOrientation(event) {
   var absolute = event.absolute;
