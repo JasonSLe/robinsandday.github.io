437021d0151018ad25461f55e276ea43a9aadf27
HynekHruska <hynek@hruska.cz>
2020-12-19 13:16:15 +0100

    dev

--- knackjs/christest.js
+++ knackjs/christest.js
@@ -195,7 +195,7 @@ const constraints = {
 
       const track = mediaStream.getVideoTracks()[0];
 
-      await track.applyConstraints(constraints);
+      track.applyConstraints(constraints);
 
       //alert(JSON.stringify(track.getConstraints()))
 
