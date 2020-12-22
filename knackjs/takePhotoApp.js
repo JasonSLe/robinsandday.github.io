15e3cadf1f4e1b79e9d04b5b7559188f8f6c9549
HynekHruska <hynek@hruska.cz>
2020-12-22 07:47:59 +0100

    dev

--- knackjs/takePhotoApp.js
+++ knackjs/takePhotoApp.js
@@ -213,6 +213,11 @@ const constraints = {
  const ctx = canvas.getContext('2d');
  let interval = 0;
  const effect = $('#cameraOverlayCanvas');
+ ctx.beginPath();
+  ctx.lineWidth = "6";
+  ctx.strokeStyle = "red";
+  ctx.rect(5, 5, 290, 140);
+  ctx.stroke();
 
  const image = new Image('naturalWidth', 'naturalHeight');
  image.onload = drawImageActualSize;
