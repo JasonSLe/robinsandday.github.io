ecd0bf7a5f4685ccc5017ac65de9ad21fc05bc6b
HynekHruska <hynek@hruska.cz>
2021-01-02 22:09:53 +0100

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -735,13 +735,9 @@ function scanDocsLinkFunction(selector_view){
 
 $(document).on("knack-scene-render.any", function(event, scene, data) {
   if (window.location.href.includes('pdfAssetId')){
-    alert('new Asset id')
-    alert(window.location.href);
     let pdfAssetField = window.location.href.substr(window.location.href.indexOf('pdfAssetField=')+14,100);
     pdfAssetField = pdfAssetField.substr(0,pdfAssetField.indexOf('&'));
-    alert(pdfAssetField);
     let pdfAssetId = window.location.href.substr(window.location.href.indexOf('pdfAssetId=')+11,100);
-    alert(pdfAssetId)
     $('input[name="'+pdfAssetField+'"]').val(pdfAssetId);
     $('div[id="kn-input-'+pdfAssetField+'"] div[class="kn-asset-current"]').attr('style',"background-color: rgba(255, 204, 153, 0);")
     $('div[id="kn-input-'+pdfAssetField+'"] div[class="kn-asset-current"]').html('created.pdf')
