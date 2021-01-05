7c9dfc0b44c4ea1103a0004408ed10e1c04d40a8
HynekHruska <hynek@hruska.cz>
2021-01-05 20:26:17 +0100

    dev

--- knackjs/christest.js
+++ knackjs/christest.js
@@ -374,7 +374,7 @@ if ( window.DeviceMotionEvent && typeof window.DeviceMotionEvent.requestPermissi
 
 setTimeout(function() {
   if (permissionForOrientation==='need'){
-    alert('need perm3')
+    //alert('need perm3')
     $('#cameraModal').show(); // show dialog asking user to enable motion sensor
     //$("#takePhoto").attr("disabled", true);//De-activate takephoto button until user agnet agreed
    $("#takePhoto").hide();
