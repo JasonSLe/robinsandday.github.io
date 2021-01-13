5547ea62e3b687d276182bbbc9d5feec383732e1
HynekHruska <hynek@hruska.cz>
2021-01-13 10:20:05 +0100

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -801,9 +801,9 @@ function embedScanApp(href){
   if ($('#jsPDF').length===0){
     loadScript('https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.2.0/jspdf.umd.min.js','jsPDF', emptyCallback)
   }
-  /*if (OperatingSystem.Android()) {
+  //if (OperatingSystem.Android()) {
    loadPinchZoomer();
-  }*/
+  //}
 }
 
 function showScanApp(){
