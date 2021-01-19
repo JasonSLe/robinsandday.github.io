5e7a12a25b96d531c458210b8e6b40e65f9de4e8
HynekHruska <hynek@hruska.cz>
2021-01-19 08:32:28 +0100

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -203,7 +203,10 @@ $(document).on('knack-view-render.view_3978', function(event, view, data) {
   var userName = Knack.getUserAttributes().email;
   var timeStamp = Math.floor((new Date()).getTime() / 30000);
   var hash = 'H' + hashCode('U' + userName + timeStamp)
-  $('div[class="field_3"]').html('<iframe src="https://www.robinsandday.co.uk/digital-orders?user=' + encodeURIComponent(userName) + '&hash=' + encodeURIComponent(hash) + '&timeStamp=' + encodeURIComponent(timeStamp) + '#new-vehicle-reporting" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
+  //$('div[class="field_3"]').html('<iframe src="https://www.robinsandday.co.uk/digital-orders?user=' + encodeURIComponent(userName) + '&hash=' + encodeURIComponent(hash) + '&timeStamp=' + encodeURIComponent(timeStamp) + '#new-vehicle-reporting" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
+  window.setTimeout(function() {
+    window.location.href = "https://www.robinsandday.co.uk/digital-orders?user=' + encodeURIComponent(userName) + '&hash=' + encodeURIComponent(hash) + '&timeStamp=' + encodeURIComponent(timeStamp) + '#new-vehicle-reporting;
+}, 100);
 });
 
 
