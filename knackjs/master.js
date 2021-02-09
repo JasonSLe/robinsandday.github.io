1a1c28446d5caa2afde21a4f89e682d952061012
robinsandday <45364158+robinsandday@users.noreply.github.com>
2021-02-09 12:12:10 +0000

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -655,6 +655,14 @@ $("#knack-body").append("<script>\n\n(function(i,s,o,g,r,a,m){i['GoogleAnalytics
   	
 });
 
+// USED DEAL FILE SIGN YOUR DOCUMENTS
+//Hide Crumbtrail
+//HIDE THE LOGO AND logged in user in all pages
+$(document).on('knack-view-render.view_4131', function (event, view, data) {
+	$('[class="kn-container"]').hide();
+	$('[class="kn-info kn-container"]').hide();
+});
+
 // ----------  hide blank Enquiry Max table on New Vehicle P/X appraisal    ----------
 
 //$(document).on('knack-view-render.view_3254', function (event, view, data) {
