11ad3db7b3e4f6db6f3b7bbf067905a6960fa933
HynekHruska <hynek@hruska.cz>
2021-04-20 23:58:46 +0200

    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -58,16 +58,10 @@ $(document).on("knack-scene-render.scene_22", function(event, scene, data) {
     console.log('set refresh 2')
     setTimeout(function(){
         console.log('refresh model reload')
-        /*
-        var a = {};
-        a.success = function () {
+        const a = {success = function () {
             console.log('render')
             Knack.views["view_75"].render()
-        }
-        Knack.views["view_75"].model.fetch(a)*/
-        Knack.views["view_75"].model.fetch().then(function() {
-            console.log('render2');
-            Knack.views["view_75"].render();
-        })
+        }};
+        Knack.views["view_75"].model.fetch(a)
     }, 5000);
   });
