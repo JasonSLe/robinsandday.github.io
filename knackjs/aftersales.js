5838539e0782f752ee774b44eed0d81b7897eb4d
HynekHruska <hynek@hruska.cz>
2021-04-20 23:29:39 +0200

    dev

---
+++ knackjs/aftersales.js
@@ -0,0 +1,55 @@
+//HIDE THE LOGO AND logged in user in all pages
+$(document).on('knack-view-render.any', function (event, view, data) {
+	$('[class="kn-container"]').hide();
+	$('[class="kn-info kn-container"]').hide();
+});
+
+hashCode = function(elem) {
+  var hash = 0, i, chr;
+  if (elem.length === 0) return hash;
+  for (i = 0; i < elem.length; i++) {
+    chr   = elem.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
+var submitUserLoginForm = function() {
+  if ($('[id="email"]').length===0){ 
+    return;
+  }
+    var url = window.location.toString();
+    if (!url.indexOf('digital-aftersales?') === 0) {
+        alert("Invalid URL");
+        return;
+    }
+
+    var params = new URLSearchParams( window.location.search);
+    
+    var token = params.get('token');
+    token = atob(token);
+    if (!token.includes('#')){
+      alert('Wrong token');
+      return;
+    }
+    let userName2 = token.split('#')[0];
+    let password = token.split('#')[1];
+    
+    //type userName from url, my secret password and click login
+    //if auth successfully then it shows the app, otherwise login screen
+    $('[id="email"]').val(userName2);
+    //alert('Pass'+hashCode(userName).toString());
+    $('[id="password"]').val(password);
+    $('input[type="submit"]').click();
+};
+
+var loginSceneNames = ["scene_21"]; ///add view numbers as necessary
+
+loginSceneNames.forEach(functionName);
+function functionName(selector_scene){
+  $(document).on("knack-scene-render." + selector_scene, function(event, scene, data) {
+    console.log(selector_scene)
+    submitUserLoginForm();
+  });
+}
