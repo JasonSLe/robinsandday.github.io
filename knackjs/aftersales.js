97e7eb1117120c5e6dc723f2cb1e8aa2038fde3f
HynekHruska <hynek@hruska.cz>
2021-04-21 14:08:05 +0200

    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -54,51 +54,83 @@ function functionName(selector_scene){
   });
 }
 
-function lookupSceneRefresh(){
+/*
+  Checks data acording to refreshData structure and updates views
+  This is structure describing the page, consisting of different views, updated with different background processes
+  Each record in refreshData represents one background update process
+  Field mainField is knack fields on first view of views array and this field is used for checking if the background process finished it run and updated the record, so the update process needs ALWAYS give some value to this field!
+  Function updates all views in views property of record, if field mainField is blank, till there is some value in mainField 
+  !mainField needs to be on first View in array!
+
+  This is just example
+  let refreshData = [
+          //mainField needs to be on first View in array
+          {
+              mainField : 'field_4',
+              views:['75','78']   
+          },{
+              mainField : 'field_74',
+              views:['76']
+          }
+        ]
+*/
+function lookupSceneRefresh(refreshData){
     console.log('lookupSceneRefresh');
-    let refreshData = [
-        //mainField needs to be on first View in array
-        {
-            mainField : 'field_4',
-            views:['75','78']   
-        },{
-            mainField : 'field_74',
-            views:['76']
-        }
-    ]
-    let recheck = false;
-    for (one of refreshData){
-        console.log(one);
-        console.log('main field val',Knack.views['view_'+one.views[0]].model.attributes[one.mainField])
-        if (Knack.views['view_'+one.views[0]].model.attributes[one.mainField]===''){
-            for (oneView of one.views){
-                refreshView(oneView);
-            }
-            console.log('main field val2',Knack.views['view_'+one.views[0]].model.attributes[one.mainField])
-            if (Knack.views['view_'+one.views[0]].model.attributes[one.mainField]===''){
-                recheck = true;
-            }
-        }
-    }
-    if (recheck){
-        console.log('needs recheck')
-        setTimeout(function(){
-            lookupSceneRefresh();
-        }, 2500);
+    try {
+      let recheck = false;
+      for (one of refreshData){
+          console.log(one);
+          console.log('main field val',Knack.views['view_'+one.views[0]].model.attributes[one.mainField])
+          if (Knack.views['view_'+one.views[0]].model.attributes[one.mainField]===''){
+              for (oneView of one.views){
+                  refreshView(oneView);
+              }
+              console.log('main field val2',Knack.views['view_'+one.views[0]].model.attributes[one.mainField])
+              if (Knack.views['view_'+one.views[0]].model.attributes[one.mainField]===''){
+                  recheck = true;
+              }
+          }
+      }
+      if (recheck){
+          //console.log('needs recheck')
+          setTimeout(function(){
+              lookupSceneRefresh();
+          }, 2500);
+      }
+    } catch (e){
+      console.log('lookupSceneRefresh fail', refreshData, e)
     }
 }
 
+//This function refreshes view acording viewId, what is just view number!
+//Can be called from scene render, view render
 function refreshView(viewID){
     console.log('refresh view', viewID)
-    const a = {}
-    a.success = function () {
+    try {
+      const a = {}
+      a.success = function () {
+        //refresh view on page
         Knack.views['view_'+viewID].render()
-    };
-    Knack.views['view_'+viewID].model.fetch(a)
+      };
+      //reload data from database
+      Knack.views['view_'+viewID].model.fetch(a)
+    } catch (e){
+      console.log('error refreshing view', viewID, e)
+    }
 }
 
 $(document).on("knack-scene-render.scene_22", function(event, scene, data) {
+    //first check after 3 seconds, but it can do the first check immediatelly
     setTimeout(function(){
-        lookupSceneRefresh();
+        let refreshData = [
+          {
+              mainField : 'field_4',
+              views:['75','78']   
+          },{
+              mainField : 'field_74',
+              views:['76']
+          }
+        ]
+        lookupSceneRefresh(refreshData);
     }, 3000);
   });
