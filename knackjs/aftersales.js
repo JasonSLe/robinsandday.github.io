f41052e54a795571f97114788d4866c1589316e3
HynekHruska <hynek@hruska.cz>
2021-04-29 06:56:08 +0200

    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -275,9 +275,9 @@ $(document).on("knack-scene-render.scene_22", function(event, scene, data) {
 
   $(document).on('knack-form-submit.view_71', function(event, view, data) { 
     let commandURL = "https://hook.integromat.com/53yx2tuy820lvzuobdqex8jem2utgwil" ;
-    let dataToSend = JSON.stringify({"id":data.id,"regNumber":data.field_31_raw,"source":"NEWRECORD"})
+    //let dataToSend = JSON.stringify({"id":data.id,"regNumber":data.field_31_raw,"source":"NEWRECORD"})
     //or theoretically to have all data from form 
-    //let dataToSend = Object.assign(data,{"source":"NEWRECORD"}); 
+    let dataToSend = Object.assign({"source":"NEWRECORD"}, data); 
     var rData = $.ajax({
       url: commandURL,
       type: 'POST',
