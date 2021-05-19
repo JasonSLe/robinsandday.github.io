1363e84a677daf5f75516a47f9bcdd3b42ae981f
HynekHruska <hynek@hruska.cz>
2021-05-19 19:22:00 +0200

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -227,12 +227,12 @@ $(document).on('knack-view-render.view_4272', function(event, view, data) {
 
 $(document).on('knack-view-render.view_4294', function(event, view, data) {
   var token = Knack.getUserAttributes().values["field_6440"];
-  $('div[class="field_3"]').html('<iframe src="https://salesjourney2.knack.com/digital-orders?token='+encodeURIComponent(token) + '#new-deal-files-automotive-compliance" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
+  $('div[class="field_3"]').html('<iframe src="https://www.robinsandday.co.uk/digital-orders?token='+encodeURIComponent(token) + '#new-deal-files-automotive-compliance" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
 });
 
 $(document).on('knack-view-render.view_4295', function(event, view, data) {
   var token = Knack.getUserAttributes().values["field_6440"];
-  $('div[class="field_3"]').html('<iframe src="https://salesjourney2.knack.com/digital-orders?token='+encodeURIComponent(token) + '#new-deal-file-manager-view" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
+  $('div[class="field_3"]').html('<iframe src="https://www.robinsandday.co.uk/digital-orders?token='+encodeURIComponent(token) + '#new-deal-file-manager-view" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
 });
 
 function openTab(url) {
