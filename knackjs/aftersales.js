d1e25de99caf3d830e14e3f67b3be3828461979f
HynekHruska <hynek@hruska.cz>
2021-05-20 22:20:46 +0200

    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -52,7 +52,7 @@ loginSceneNames.forEach(functionName);
 function functionName(selector_scene){
   $(document).on("knack-scene-render." + selector_scene, function(event, scene, data) {
     //console.log(selector_scene)
-    submitUserLoginForm();
+    //submitUserLoginForm();
   });
 }
 
