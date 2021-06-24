9d8a2e2fda9f099343a7f4ad9fb02d6035053845
robinsandday <45364158+robinsandday@users.noreply.github.com>
2021-06-24 09:55:57 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -1951,7 +1951,7 @@ $(document).on('knack-form-submit.view_4166', function(event, view, data) {
 // Used Deal File - Capture PDFs TRIGGER INTEGROMAT UPON – *Used Deal File PDF - Order Form signed on site {(Deal File) Used Vehicle Deal File} Replaces https://zapier.com/app/editor/103142236?redirect=true
 $(document).on('knack-form-submit.view_2531', function(event, view, data) {
 	
-	console.log("Test 1");
+	console.log("Test 2");
 	console.log(typeof data.field_5441_raw);
         console.log(typeof data.field_5957_raw);
     
