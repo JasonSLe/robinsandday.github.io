887ff0ffe6b0d30333352ffb7ef99481b7db5f24
HynekHruska <hynek@hruska.cz>
2021-06-25 19:23:24 +0200

    dev

--- knackjs/orders.js
+++ knackjs/orders.js
@@ -514,4 +514,6 @@ $(document).on('knack-form-submit.view_2765', function(event, view, data) {
   console.log(rData);
 });
 
-
+$(document).on('knack-view-render.view_3633', function(event, view, data) {
+  $('div[id="view_3633"] p[class="kn-description"]').text($('div[id="view_3633"] p[class="kn-description"]').text()+'<br />test')
+});
