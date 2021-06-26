a5e6de7a1ed428711ca06811abb4f5672490c8ff
HynekHruska <hynek@hruska.cz>
2021-06-26 08:33:33 +0200

    dev

--- knackjs/orders.js
+++ knackjs/orders.js
@@ -514,7 +514,7 @@ $(document).on('knack-form-submit.view_2765', function(event, view, data) {
   console.log(rData);
 });
 
-$(document).on('knack-view-render.view_3633', function(event, view, data) {\
+$(document).on('knack-view-render.view_3633', function(event, view, data) {
   function dateTimeToGB(dateobj){
     return pad(dateobj.getDate())+"/"+pad(dateobj.getMonth()+1)+"/"+dateobj.getFullYear()+' '+pad(dateobj.getHours())+':'+pad(dateobj.getMinutes());
   }
