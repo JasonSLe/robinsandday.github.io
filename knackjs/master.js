cbf02c4c730db49ff99ddeb51058e6c729dda2c0
robinsandday <45364158+robinsandday@users.noreply.github.com>
2021-06-30 15:15:48 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -2499,8 +2499,7 @@ $(document).on('knack-form-submit.view_426', function(event, view, data) {
 
 // Enquiry Max – **Instant trigger from TRADE Or Offsite P/X appraisal completion to Integromat to return data to Enquiry Max {(P/X) Part Exchange Vehicles} - Replaces https://zapier.com/app/editor/81416151?redirect=true
 $(document).on('knack-form-submit.view_370', function(event, view, data) { 
-
-	console.log("Test 5");
+   console.log("Test 6");
 	let commandURL = "https://hook.integromat.com/71nekxpf0if53hc6gauk8j2rc3wqiv7p" ;
   let dataToSend = {"Knack UID":data.id,"VRM":data.field_257_raw,"Odometer":data.field_258_raw,"Main Image":data.field_532_raw,
   "URL to Access Valuation":"https://www.robinsandday.co.uk/digital#new-appraisal/offsite-or-trade-valuation/" + data.id + "/","Valuation": "£" + data.field_753_raw,
@@ -2513,7 +2512,13 @@ $(document).on('knack-form-submit.view_370', function(event, view, data) {
   let newJSON = JSON.stringify(dataToSend, function (key, value) {return (value === undefined) ? "1" : value;});
  
 
-  console.log(JSON.parse(newJSON))
+  var rData = $.ajax({
+    url: commandURL,
+    type: 'POST',
+    contentType: 'application/json',
+    data: newJSON,
+    async: false
+  }).responseText;
   
 });
 
