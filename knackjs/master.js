96d7cd22156ba322cd0b46e621420119849c8c98
robinsandday <45364158+robinsandday@users.noreply.github.com>
2021-07-09 10:44:53 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -3339,3 +3339,39 @@ $(document).on('knack-form-submit.view_2680', function(event, view, data) {
 });
 
 
+// New Deal File - Automated Comms – **New Deal File Automated Comms - Profit & Loss Uploaded {(Deal File) Digital Deal File} Slave App- Replaces https://zapier.com/app/editor/102172889?redirect=true
+$(document).on('knack-form-submit.view_2602', function(event, view, data) { 
+    
+    try{
+
+        let commandURL = "https://hook.integromat.com/27gimyyfnsdz3jfji1q5b4ag65xx9wzc";
+        let dataToSend = JSON.stringify({"Record ID":data.id, "Trigger":"Profit & Loss Uploaded", "Source Of Payload":"knack direct"});
+
+      var rData = $.ajax({
+        url: commandURL,
+        type: 'POST',
+        contentType: 'application/json',
+        data: dataToSend,
+        async: false
+      }).responseText;
+  
+    }catch(exception){
+        console.log("error");
+        var today = new Date();
+        var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+
+        let commandURL = "https://hook.integromat.com/bxfn25wkj67pptq9bniqmpvvjg868toi";
+        let dataToSend = JSON.stringify({"Source":"Javascript error", "Function": "New Deal File Automated Comms - Profit & Loss Uploaded {(Deal File) Digital Deal File} Slave App",
+        "Payload": data, "userName": Knack.getUserAttributes().name, "userEmail": Knack.getUserAttributes().email, "Exception": exception.message, "dateTime": dateTime});
+        var rData = $.ajax({
+           url: commandURL,
+           type: 'POST',
+           contentType: 'application/json',
+           data: dataToSend,
+           async: false
+        }).responseText;
+    } 
+});
+
