ea78f25f4bfe81abacb29f6e64cdf1a45f4639f9
robinsandday <45364158+robinsandday@users.noreply.github.com>
2021-07-12 11:16:08 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -3755,3 +3755,41 @@ $(document).on('knack-form-submit.view_2822', function(event, view, data) {
     }
 });
 
+
+// New Deal File - Capture PDFs – **New Deal File PDF - Part Ex Purchase Invoice signed online by Customer {(Deal File) Customer Part Exchange Invoice} Slave App - Replaces https://zapier.com/app/editor/116189304?redirect=true
+$(document).on('knack-form-submit.view_3683', function(event, view, data) {
+    
+    try{
+
+        let commandURL = "https://hook.integromat.com/ue6mctvmfbukksn2battr5cqtgnx135v";
+        let dataToSend = JSON.stringify({"Record ID":data.id, "Form":"Part exchange purchase invoice", "Source Of Payload":"knack direct"});
+
+
+      var rData = $.ajax({
+        url: commandURL,
+        type: 'POST',
+        contentType: 'application/json',
+        data: dataToSend,
+        async: false
+      }).responseText;
+  
+    }catch(exception){
+        console.log("error");
+        var today = new Date();
+        var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+
+        let commandURL = "https://hook.integromat.com/bxfn25wkj67pptq9bniqmpvvjg868toi";
+        let dataToSend = JSON.stringify({"Source":"Javascript error", "Function": "New Deal File - Capture PDFs – **New Deal File PDF - Part Ex Purchase Invoice signed online by Customer {(Deal File) Customer Part Exchange Invoice} Slave App",
+        "Payload": data, "userName": Knack.getUserAttributes().name, "userEmail": Knack.getUserAttributes().email, "Exception": exception.message, "dateTime": dateTime});
+        var rData = $.ajax({
+           url: commandURL,
+           type: 'POST',
+           contentType: 'application/json',
+           data: dataToSend,
+           async: false
+        }).responseText;
+    }
+});
+
