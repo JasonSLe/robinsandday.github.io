6e96f8c67f3a00e12f5e13d3a7d3ecf3fb796f9d
robinsandday <45364158+robinsandday@users.noreply.github.com>
2021-07-12 13:45:07 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -3951,3 +3951,40 @@ $(document).on('knack-form-submit.view_3680', function(event, view, data) {
     }
   
 });
+
+
+//**New Deal File PDF - Merge POST Sale Pack and Customer Signature {(Deal File) Digital Deal File} Slave App https://zapier.com/app/editor/116785934?redirect=true
+$(document).on('knack-form-submit.view_3696', function(event, view, data) { 
+    
+    try{
+    
+
+        let commandURL = "https://hook.integromat.com/6aee4yo8i2g1chdo9hphjnaw4n42ldor";
+        let dataToSend = JSON.stringify({"Record ID":data.id, "Form":"Post Sale Pack", "Source Of Payload":"knack direct"});
+
+          var rData = $.ajax({
+            url: commandURL,
+            type: 'POST',
+            contentType: 'application/json',
+            data: dataToSend,
+            async: false
+          }).responseText;
+    }catch(exception){
+        console.log("error");
+        var today = new Date();
+        var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+
+        let commandURL = "https://hook.integromat.com/bxfn25wkj67pptq9bniqmpvvjg868toi";
+        let dataToSend = JSON.stringify({"Source":"Javascript error", "Function": "New Deal File PDF - Merge POST Sale Pack and Customer Signature {(Deal File) Digital Deal File} Slave App",
+        "Payload": data, "userName": Knack.getUserAttributes().name, "userEmail": Knack.getUserAttributes().email, "Exception": exception.message, "dateTime": dateTime});
+        var rData = $.ajax({
+           url: commandURL,
+           type: 'POST',
+           contentType: 'application/json',
+           data: dataToSend,
+           async: false
+        }).responseText;
+    }
+});
