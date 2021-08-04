2818facc7d356800c60a2be1c7461d6bb6b2bf55
HynekHruska <hynek@hruska.cz>
2021-08-04 12:29:05 +0200

    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -466,7 +466,7 @@ $(document).on("knack-scene-render.scene_29", function(event, scene, data) {
 
   $(document).on('knack-view-render.view_140', function (event, view, data) {
     let button0 = document.createElement('button');
-    button0.innerHTML = 'With Autoline stock';
+    button0.innerHTML = 'In Stock at All Hubs';
     button0.setAttribute("class", "kn-button");
     button0.onclick = function(){
       let token = getTokenFromURL(document.location.href);
@@ -484,7 +484,7 @@ $(document).on("knack-scene-render.scene_29", function(event, scene, data) {
     };
     document.getElementById('view_140').appendChild(button1)
     let button2 = document.createElement('button');
-    button2.innerHTML = 'Has Hub In Stock';
+    button2.innerHTML = 'In stock at own hub';
     button2.setAttribute("class", "kn-button");
     button2.onclick = function(){
       let token = getTokenFromURL(document.location.href);
@@ -501,7 +501,15 @@ $(document).on("knack-scene-render.scene_29", function(event, scene, data) {
       return false;
     };
     document.getElementById('view_140').appendChild(button3)
-    
+    let button4 = document.createElement('button');
+    button4.innerHTML = 'In stock at own hub, with BON Raised';
+    button4.setAttribute("class", "kn-button");
+    button4.onclick = function(){
+      let token = getTokenFromURL(document.location.href);
+      document.location = "https://www.robinsandday.co.uk/aftersales#powersupply-orders/?token="+token+"&view_139_page=1&view_139_sort=field_334|desc&view_139_filters=%7B%22match%22%3A%22and%22%2C%22rules%22%3A%5B%7B%22field%22%3A%22field_336%22%2C%22operator%22%3A%22is%20not%20blank%22%2C%22field_name%22%3A%22Power%20Supply%20Notfication%22%7D%2C%7B%22field%22%3A%22field_345%22%2C%22operator%22%3A%22is%22%2C%22value%22%3A%22Yes%22%2C%22field_name%22%3A%22Has%20Hub%20In%20Stock%22%7D%5D%7D"
+      return false;
+    };
+    document.getElementById('view_140').appendChild(button4)
   });
 
   
