872a74967b1d690e3bcf1fad5cbaf81b2105e6b5
robinsandday <45364158+robinsandday@users.noreply.github.com>
2021-08-11 15:38:58 +0100

    Update orders.js

--- knackjs/orders.js
+++ knackjs/orders.js
@@ -797,17 +797,22 @@ $(document).on('knack-form-submit.view_2630', function(event, view, data) {
     try{
         
  
-
+    console.log("Test 1");
+    console.log(data.field_7197_raw);
     // Searching an undefined collection/aray will result in an exception and the javascript will stop execution!
     function handlAll(valueA, fieldName){ 
         return (valueA? valueA[fieldName]:"");//This tests if valueA is not null or undefined, if yes it returns empty string, otherwise it returns property of fieldName of valueA
     }
     
     function handlArrayID(valueA, indexNumber, fieldName){
-        if(valueA === "undefined" || valueA === null){
+        console.log("The value of the if statement :" + valueA !== "undefined" && valueA !== null);
+        if(valueA !== "undefined" && valueA !== null){
+            console.log("The valueA: " + valueA);
+            return valueA.length > 0? valueA[indexNumber][fieldName]:"";
+            
+        }else{
             return "";
         }
-        return valueA.length > 0? valueA[indexNumber][fieldName]:"";
     }
     
     function handlArray(valueA){
