ab7cb29c53b859246bb56c98399b7b27de96fdfc
HynekHruska <hynek@hruska.cz>
2021-08-13 07:28:07 +0200

    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -525,17 +525,23 @@ $(document).on("knack-scene-render.scene_29", function(event, scene, data) {
   $(document).on('knack-view-render.view_151', function (event, view, data) {
     console.log('view_151')
     if (Notification.permission !== 'granted') Notification.requestPermission();
+    /*
     var notification = new Notification('Notification title', {
       icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
       body: 'Hey there! You\'ve been notified!',
-      requireInteraction: true,
-      actions: [  
-        {action: 'like', title: 'Like'},  
-        {action: 'reply', title: 'Reply'}]
+      requireInteraction: true
      });
-     /*notification.onclick = function() {
+     notification.onclick = function() {
       window.open('http://stackoverflow.com/a/13328397/1269037');
      };*/
+     navigator.serviceWorker.ready.then(function(registration) {
+      registration.showNotification('Vibration Sample', {
+        body: 'Buzz! Buzz!',
+        icon: '../images/touch/chrome-touch-icon-192x192.png',
+        vibrate: [200, 100, 200, 100, 200, 100, 200],
+        tag: 'vibration-sample'
+      });
+    });
   });
 
   
