98679b8b294bb1ebc0eed74f323c320a5219a552
robinsandday <45364158+robinsandday@users.noreply.github.com>
2021-08-17 16:39:35 +0100

    Update orders.js

--- knackjs/orders.js
+++ knackjs/orders.js
@@ -1985,46 +1985,16 @@ $(document).on('knack-form-submit.view_3968', function(event, view, data) {
                 data: dataToSend,
                 async: false
               }).responseText;
+              
+              
+             let commandURL1 = "https://hook.integromat.com/lnunp83lom13c9swu0vgabmurbjxj5x6";
+            let dataToSend1 = JSON.stringify({"Record ID":data.id, "Form":"Customer satisfaction survey", "Source Of Payload":"knack direct"});
 
-      }
-    }catch(exception){
-        console.log("error");
-        var today = new Date();
-        var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        var dateTime = date+' '+time;
-
-        let commandURL = "https://hook.integromat.com/bxfn25wkj67pptq9bniqmpvvjg868toi";
-        let dataToSend = JSON.stringify({"Source":"Javascript error", "Function": "New Deal File PDF - Customer satisfaction survey signed at dealer V2 {(Deal File) Customer Satisfaction Survey} Slave App",
-        "Payload": data, "userName": Knack.getUserAttributes().name, "userEmail": Knack.getUserAttributes().email, "Exception": exception.message, "dateTime": dateTime});
-        var rData = $.ajax({
-           url: commandURL,
-           type: 'POST',
-           contentType: 'application/json',
-           data: dataToSend,
-           async: false
-        }).responseText;  
-    }
-});
-
-
-// New Deal File - Capture PDFs – **New Deal File PDF - Customer satisfaction survey VX signed at dealer V2 {(Deal File) Customer Satisfaction Survey} Slave App - Replaces https://zapier.com/app/editor/116188221?redirect=true
-$(document).on('knack-form-submit.view_3968', function(event, view, data) { 
-    
-    
-    try{
-        
-
-      if(data.field_6485_raw !== null && data.field_6485_raw !== undefined){
-
-            let commandURL = "https://hook.integromat.com/ue6mctvmfbukksn2battr5cqtgnx135v";
-            let dataToSend = JSON.stringify({"Record ID":data.id, "Form":"Customer satisfaction survey", "Source Of Payload":"knack direct"});
-
-             var rData = $.ajax({
-                url: commandURL,
+             var rData1 = $.ajax({
+                url: commandURL1,
                 type: 'POST',
                 contentType: 'application/json',
-                data: dataToSend,
+                data: dataToSend1,
                 async: false
               }).responseText;
 
