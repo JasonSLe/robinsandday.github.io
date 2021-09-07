500e701b853cbadc238f970faaa60dc86cbc3344
robinsandday <45364158+robinsandday@users.noreply.github.com>
2021-09-07 14:37:00 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -1613,6 +1613,16 @@ $(document).on('knack-record-update.view_4086', function(event, view, data) {
   
 });
 
+
+/***Profit and Loss, refresh after warranty cost has been entered by user (DIGITAL P&L)***/
+$(document).on('knack-record-update.view_4562', function(event, view, data) {
+  
+  setTimeout(function () { location.hash = location.hash + "#"; }, 1000);
+  Knack.showSpinner();
+	
+  
+});
+
 /***Profit and Loss, refresh after warranty cost has been entered by user***/
 $(document).on('knack-record-update.view_4589', function(event, view, data) {
   
