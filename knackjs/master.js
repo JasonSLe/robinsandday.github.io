0e4d27178bbd6d2c35db19af64db1a757740b67b
HynekHruska <hynek@hruska.cz>
2021-10-13 11:41:59 +0200

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -764,6 +764,16 @@ $(document).on('knack-scene-render.scene_1314', function (event, view, data) {
 	$('[class="kn-info kn-container"]').hide();
 });
 
+$(document).on('knack-scene-render.scene_1314', function (event, view, data) {
+	$('[class="kn-container"]').hide();
+	$('[class="kn-info kn-container"]').hide();
+});
+
+$(document).on('knack-scene-render.scene_1497', function (event, view, data) {
+	$('[class="kn-container"]').hide();
+	$('[class="kn-info kn-container"]').hide();
+});
+
 
 // ----------  hide blank Enquiry Max table on New Vehicle P/X appraisal    ----------
 
