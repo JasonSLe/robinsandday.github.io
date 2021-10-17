4e503aa87f720daa041456119a59a1305d0b2468
HynekHruska <hynek@hruska.cz>
2021-10-17 07:43:35 +0200

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -291,11 +291,9 @@ $(document).on('knack-scene-render.any', function(event, scene) {
     toggleButton.addEventListener('click', toggleHeaderVisibility);
 
     // Always hide the header when rendering scene in IFRAME, add other scenes if necessary
-    if (scene.key == 'scene_860' || scene.key == 'scene_1269' || scene.key == 'scene_1271' || scene.key == 'scene_1273' || scene.key == 'scene_1279' || scene.key == 'scene_1339' || scene.key == 'scene_1362' || scene.key == 'scene_1412' || scene.key == 'scene_1497') {
+    if ((scene.key == 'scene_860' || scene.key == 'scene_1269' || scene.key == 'scene_1271' || scene.key == 'scene_1273' || scene.key == 'scene_1279' || scene.key == 'scene_1339' || scene.key == 'scene_1362' || scene.key == 'scene_1412' || scene.key == 'scene_1497') && headerElement.style.display ==! 'none') {
         if (window.matchMedia('(min-width: 768px)').matches !== false) {
-          $(document).ready(function(){
             toggleHeaderVisibility('hidden');
-          });
         }
     }
   
