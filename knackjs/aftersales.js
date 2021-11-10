8bf541bbf5a2c8a67ca5f985f11690b80fe149e4
robinsandday <45364158+robinsandday@users.noreply.github.com>
2021-11-10 14:34:08 +0000

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -751,3 +751,39 @@ try{
     }).responseText;
 }
 });
+
+//trigger Aftersales Tyre dealer Stock Lookup
+
+$(document).on('knack-form-submit.view_230', function(event, view, data) {
+
+try{
+
+    let commandURL = "https://hook.integromat.com/tjsfrxojnnvd3byqy1fr3qobk8pflvkf";
+    let dataToSend = JSON.stringify({"Record ID":data.id});
+
+    var rData = $.ajax({
+        url: commandURL,
+        type: 'POST',
+        contentType: 'application/json',
+        data: dataToSend,
+        async: false
+    }).responseText;    
+}catch(exception){
+    console.log("error");
+    var today = new Date();
+    var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+
+    let commandURL = "https://hook.integromat.com/bxfn25wkj67pptq9bniqmpvvjg868toi";
+    let dataToSend = JSON.stringify({"Source":"Javascript error", "Function": "Scenario DESCRIPTION what for the error webhook",
+    "Payload": data, "userName": Knack.getUserAttributes().name, "userEmail": Knack.getUserAttributes().email, "Exception": exception.message, "dateTime": dateTime});
+    var rData = $.ajax({
+       url: commandURL,
+       type: 'POST',
+       contentType: 'application/json',
+       data: dataToSend,
+       async: false
+    }).responseText;
+}
+});
