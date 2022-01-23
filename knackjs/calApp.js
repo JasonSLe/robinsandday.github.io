7481bd93a941c4c2859e808ad8b5d824f78f7e6a
HynekHruska <hynek@hruska.cz>
2022-01-23 21:09:33 +0100

    dev

--- knackjs/calApp.js
+++ knackjs/calApp.js
@@ -114,7 +114,31 @@ if (OperatingSystem.Android()) {
 
   calibrateButton.onclick = function(){
     alert(lastBeta);
+    setCookie('rdSpiritCalibration', lastBeta,365);
   }
+
+  function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
   
   //**************************** DETECT SCREEN ORIENTATION WHEN THE APP IS LOADED AND DETECT WHEN USER CHANGES SCREEN ORIENTATION*****************************************
 var isLandscape = false;
