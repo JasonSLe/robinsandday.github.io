c78dd1c0213fc45764bcdde218b22afbd1c3f1c0
HynekHruska <hynek@hruska.cz>
2022-02-01 22:20:22 +0100

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -1603,7 +1603,7 @@ var calibrationValue = getCookie('rdSpiritCalibration');
 
   if (Knack.getUserAttributes().email.includes('hynek') || Knack.getUserAttributes().email.includes('david.male') || Knack.getUserAttributes().email.includes('conor.power')){
     if (isInCalibrationMode){
-      $('#infoText').text('b:'+beta+',g:'+gamma);
+      $('#infoText').html('b:'+beta+'<br />g:'+gamma);
     }
   }
 
