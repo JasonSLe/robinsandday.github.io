9c04cc50ee4d9fa7382345dc0f47529d3eaaa184
robinsandday <45364158+robinsandday@users.noreply.github.com>
2022-02-04 15:54:19 +0000

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -2916,34 +2916,11 @@ $(document).on('knack-scene-render.scene_960', function (event, view, data) {
 });
 
 
-//**Function used in the Catch block of the API webhooks
-function handlJSExceptions(exception, functionName){ 
-        console.log("error");
-        var today = new Date();
-        var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        var dateTime = date+' '+time;
-
-        let commandURL = "https://hook.integromat.com/bxfn25wkj67pptq9bniqmpvvjg868toi";
-        let dataToSend = JSON.stringify({"Source":"Javascript error", "Function": functionName,
-        "Payload": data, "userName": Knack.getUserAttributes().name, "userEmail": Knack.getUserAttributes().email, "Exception": exception.message, "dateTime": dateTime});
-        var rData = $.ajax({
-           url: commandURL,
-           type: 'POST',
-           contentType: 'application/json',
-           data: dataToSend,
-           async: false
-        }).responseText;
-        }
-
-
-
 //**Trigger Text To Customer To Complete Exit Survey At Workshop "Check Out"
 $(document).on('knack-form-submit.view_318', function(event, view, data) { 
     
     try{
-        
-        console.log("Testing2");
+        console.log("Test 3");
         let commandURL = "https://hook.integromat.com/reyy2orzb5n7ututhmsltfcxqvkfpjmh";
         let dataToSend = JSON.stringify({"Record ID":data.id, "ExitSurveyUID":data.field_769_raw});
 
@@ -2955,6 +2932,23 @@ $(document).on('knack-form-submit.view_318', function(event, view, data) {
             async: false
         }).responseText;
     }catch(exception){
-        handlJSExceptions(exception, "Trigger Text To Customer To Complete Exit Survey At Workshop \"Check Out\"");
+        console.log("error");
+        var today = new Date();
+        var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+
+        let commandURL = "https://hook.integromat.com/bxfn25wkj67pptq9bniqmpvvjg868toi";
+        let dataToSend = JSON.stringify({"Source":"Javascript error", "Function": "Trigger Text To Customer To Complete Exit Survey At Workshop \"Check Out\"",
+        "Payload": data, "userName": Knack.getUserAttributes().name, "userEmail": Knack.getUserAttributes().email, "Exception": exception.message, "dateTime": dateTime});
+        var rData = $.ajax({
+           url: commandURL,
+           type: 'POST',
+           contentType: 'application/json',
+           data: dataToSend,
+           async: false
+        }).responseText;
     }
 });
+
+
