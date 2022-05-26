b87c8cf4b6e78818e7fc915e5c6dd0483d0d7193
robinsandday <45364158+robinsandday@users.noreply.github.com>
2022-05-26 16:08:35 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -955,4 +955,10 @@ function recursivecall(){
  setTimeout(function () { if($("#view_638").is(":visible")==true){ Knack.views["view_638"].model.fetch();recursivecall();} }, 30000);
 }
 
+// Trigger Customer Incident Form
+
+$(document).on('knack-form-submit.view_781', function(event, view, data) { 
+  callPostHttpRequest("https://hook.integromat.com/gmtkedwe7nxktiqm6qi4rg5apeno73an", {"Record ID":data.id},"Pre Visit Digital Customer Incident Form")
+});
+
 
