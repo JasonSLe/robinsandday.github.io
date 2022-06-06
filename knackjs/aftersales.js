022f65285faaa9affab15401a8c0cd09a42e0769
Alexandru Tanase <t_alexandru87@yahoo.com>
2022-06-06 12:07:36 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -1000,7 +1000,7 @@ $(document).on('knack-view-render.view_375', function(event, view, data) {
       color: "#fff",
       textShadow: "0 0 7px #fff,  0 0 10px #fff, 0 0 21px #fff, 0 0 42px #0fa, 0 0 82px #0fa, 0 0 92px #0fa, 0 0 102px #0fa, 0 0 151px #0fa"
     }
-    $(this).css("color", greenStyle);
+    $(this).css(greenStyle);
     //$(this).css("text-shadow", textShadow);
     })
 });
