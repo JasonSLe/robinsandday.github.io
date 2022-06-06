b1def23ab0ee17fb4c0758bd3cc5dfb76faba1ac
Alexandru Tanase <t_alexandru87@yahoo.com>
2022-06-06 11:18:35 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -987,5 +987,18 @@ $(document).on('knack-form-submit.view_732', function(event, view, data) {
   "Service GDPR SMS":data.field_1052_raw, "Sales GDPR PHONE":data.field_1054_raw, "Sales GDPR EMAIL":data.field_1055_raw,"Sales GDPR POST":data.field_1056_raw, "Sales GDPR SMS":data.field_1057_raw, "Customer Magic Number":data.field_1006_raw.replace(/[^0-9]/g,'')},"Submit form for GDPR preferences update in Check-in process")
 });
 
-
+//change the color based on input
+$(document).on('knack-view-render.view_375', function(event, view, data) {
+  console.log("Test1");
+  console.log($("#view_375 field_1048").val());
+  $("#view_375 .kn-label-none .kn-detail-body span").each(function() {
+	  console.log("Trigger change color")
+    // #ff0000 is red and the #1c631f is green
+    const textColor = ($(this).find("span").text().trim() == "No") ? "#fff" : "#fff";
+	  console.log(`The color is: ${textColor}`)
+    const textShadow = "0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px #0fa, 0 0 82px #0fa, 0 0 92px #0fa, 0 0 102px #0fa, 0 0 151px #0fa;"
+    $(this).css("color", textColor);
+    $(this).css("text-shadow", textShadow);
+    })
+});
 
