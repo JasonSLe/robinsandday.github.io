fd950540c1c10b864dd84f92f7c79e702d1b8992
Alexandru Tanase <t_alexandru87@yahoo.com>
2022-06-06 12:12:21 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -997,7 +997,7 @@ $(document).on('knack-view-render.view_375', function(event, view, data) {
     const textColor = ($(this).find("span").text().trim() == "No") ? "#228B22" : "#228B22";
 	  console.log(`The color is: ${textColor}`)
     const greenStyle = {
-      color: "#fff",
+      color: "#228B22",
       textShadow: "0 0 7px #fff,  0 0 10px #fff, 0 0 21px #fff, 0 0 42px #0fa, 0 0 82px #0fa, 0 0 92px #0fa, 0 0 102px #0fa, 0 0 151px #0fa"
     }
     $(this).css(greenStyle);
