369d9d7c08dc5e742fac49adf200a387dd288be8
Alexandru Tanase <t_alexandru87@yahoo.com>
2022-06-15 12:21:00 +0100

    Update aftersales.js
    Added Change Keyword Search Placeholder Text for Wip Management Search

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -1068,6 +1068,6 @@ function recursivecall(){
 
 
 /* Change Keyword Search Placeholder Text for Wip Management Search */
-$(document).on('#view_596 > div:nth-child(2) > div:nth-child(2) > form > p > input', function(event, scene) {
+$(document).on('knack-view-render.view_596 > .kn-records-nav > .level', function(event, scene) {
   $("input[name='keyword']").attr("placeholder", "Type Reg Number")
 });
