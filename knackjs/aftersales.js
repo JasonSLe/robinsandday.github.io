2e6c97527704f7cbaf254a6af8adc4883234ccb7
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2022-06-15 11:58:26 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -1068,6 +1068,6 @@ function recursivecall(){
 
 
 /* Change Keyword Search Placeholder Text for Wip Management Search */
-$(document).on('knack-view-render.view_596 > div:nth-child(2) > div:nth-child(2) > form > p > input', function(event, scene) {
+$document.querySelector("#view_596 > div:nth-child(2) > div:nth-child(2) > form > p > input"), function(event, scene) {
   $("input[name='keyword']").attr("placeholder", "Type Reg Number")
 });
