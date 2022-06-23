f73b716193089b41c1fe3c76790fb4a6d7bc727f
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2022-06-23 14:30:20 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -1050,6 +1050,17 @@ $(document).on('knack-form-submit.view_736', function(event, view, data) {
 	  //hide VIN from table
 	    $('th[class="field_73"]').hide();
     $('td[class*="field_73"]').hide();
+	  	  //hide reg
+	  $('th[class="field_31"]').hide();
+    $('td[class*="field_31"]').hide();
+	  
+	  //hide wip num
+	  $('th[class="field_441"]').hide();
+    $('td[class*="field_441"]').hide();
+	  
+	  //hide account num
+	  $('th[class="field_756"]').hide();
+    $('td[class*="field_756"]').hide();
 	  
     //This part is for column headers
     //Column header
@@ -1062,17 +1073,9 @@ $(document).on('knack-form-submit.view_736', function(event, view, data) {
   });
 //hide vin from last clocked vs invoice table
   $(document).on('knack-view-render.view_244', function (event, view, data) {
+	  
 	    $('th[class="field_622"]').hide();
     $('td[class*="field_622"]').hide();
-	  //hide reg
-	  $('th[class="field_31"]').hide();
-    $('td[class*="field_31"]').hide();
-	  //hide wip num
-	  $('th[class="field_441"]').hide();
-    $('td[class*="field_441"]').hide();
-	  //hide account num
-	  $('th[class="field_756"]').hide();
-    $('td[class*="field_756"]').hide();
 });
 // ----------  Refresh Customer Incident Form table every 60 seconds but not the page itself  ---------- //
 
