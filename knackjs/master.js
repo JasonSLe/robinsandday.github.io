4c9720b203ee7f31ce5af12f62180b79930457be
Hynek <hynek@hruska.cz>
2022-09-18 18:46:28 +0200

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -3262,7 +3262,10 @@ function ffMPEGPrepare(fieldNumber){
       var Module = {
         print: print,
         printErr: print,
-        files: [new Uint8Array(reader.result)] || [],
+        files: [{
+          "name": files[0].name,
+          "data": new Uint8Array(reader.result)
+        }] || [],
         arguments: parseArguments('-i '+files[0].name+' -vf showinfo -strict -2 output.mp4') || [],
         TOTAL_MEMORY: 268435456
       };
