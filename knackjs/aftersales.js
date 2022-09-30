31eeb440bd106d2b0524b83a9aa8fc4072544e5c
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2022-09-30 10:04:54 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -1289,3 +1289,24 @@ $(document).on('knack-record-update.view_1208', function(event, view, data) {
   Knack.showSpinner();
   
 });
+
+  // --- Aftersales vehicle look up 'vehicle on site' ---
+$(document).on('knack-view-render.view_1223', function(event, view) {
+ //send a http request with the vin an record id
+
+ const triggerRecord = (event2) => {
+  console.log("Test106")
+   console.log(event2.view.app_id)
+   console.log(event2.view.Knack)
+   let k = Object.assign({},event2.view.Knack);
+   console.log(event2.view.Knack.hash_parts)
+   console.log(k.hash_parts)
+   console.log(event2.view.Knack.scene_hash)
+   console.log(event2.view.Knack.google_loading)
+   console.log(event2.view.Knack.domain)
+  
+   callPostHttpRequest("https://hook.eu1.make.celonis.com/a61ljkqf5jw5d643274gixjtqdx5hgo8", {"Record ID":event2.view.app_id},"Aftersales- update  individual LIVE WIPS 'touched today' and UPDATE Parts & Labour v4");
+ }
+ //add an event listner to the arrow table element
+ $(".Customer and Vehicle Summary").on("click", triggerRecord)
+});
