e0d9fcaec52e311839bd3298fdfd45ee1877790f
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2022-10-04 13:33:52 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -1328,7 +1328,7 @@ $(document).on('knack-view-render.view_1223', function(event, view) {
             callPostHttpRequest("https://hook.eu1.make.celonis.com/a61ljkqf5jw5d643274gixjtqdx5hgo8", {"Record ID":cell, "VIN": vinNumber, "Scenario":"vehicle customer look up" },"Aftersales- update individual LIVE WIPS 'touched today' and UPDATE Parts & Labour v4");
           };
         };
-        currentRow.children[6].onclick = createClickHandler(currentRow);
+        currentRow.children[5].onclick = createClickHandler(currentRow);
       }
     }
 	});
