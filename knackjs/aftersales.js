a850298516d1070e9e92f1152ab4ebeb18c77ac1
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2022-10-05 13:07:27 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -1422,5 +1422,5 @@ $(document).on('knack-view-render.view_4272', function(event, view) {
    callPostHttpRequest("https://hook.integromat.com/sp402xbep1ae24s9edipuywro6wg9hk1", {"Record ID":event2.view.app_id, "VIN": vinNumber },"Aftersales- will triger during vehicle check-in");
  }
  //add an event listner to the arrow table element
- $(".span > a").on("click", triggerRecord)
+ $(".td:nth-child(2) > span > a").on("click", triggerRecord)
 });
