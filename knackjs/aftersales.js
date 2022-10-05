f8357e963837fbd0e597a3d7b44bb9349145c118
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2022-10-05 11:55:09 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -1355,9 +1355,9 @@ $(document).on("knack-scene-render.scene_105", function(event, scene, data) {
 
 
   // --- Aftersales Virtual reception update job card ---
-//$(document).on('knack-view-render.view_1169', function(event, view) {
+$(document).on('knack-view-render.view_1169', function(event, view) {
   //get the vin value from the table
-/* const vinNumber = $(".col-8").text().trim()
+ const vinNumber = $(".col-8").text().trim()
  //send a http request with the vin an record id
 
  const triggerRecord = (event2) => {
@@ -1374,24 +1374,9 @@ $(document).on("knack-scene-render.scene_105", function(event, scene, data) {
   
    callPostHttpRequest("https://hook.eu1.make.celonis.com/a61ljkqf5jw5d643274gixjtqdx5hgo8", {"Record ID":event2.view.app_id, "VIN": vinNumber, "Scenario":"vehicle customer look up" },"Aftersales- update individual LIVE WIPS 'touched today' and UPDATE Parts & Labour v4");
  }
-   //add an event listner to the arrow table element
-    $(".span").on("click", triggerRecord); */
- 
-   /* if ($('div[class="kn-view kn-table view_1169"]')){
-     let rows = $('div[class="kn-view kn-table view_1169"] table tr');
-     for (i = 1; i < rows.length; i++) {
-     let currentRow = rows[i];
-     const createClickHandler = function(row) {
-       return function() {
-      var cell = row.id;
-       console.log('cell',cell);
-       callPostHttpRequest("https://hook.eu1.make.celonis.com/a61ljkqf5jw5d643274gixjtqdx5hgo8", {"Record ID":cell, "VIN": vinNumber, "Scenario":"vehicle customer look up" },"Aftersales- update individual LIVE WIPS 'touched today' and UPDATE Parts & Labour v4");
-        };
-       };
-       currentRow.children[5].onclick = createClickHandler(currentRow);
-      }
-    }*/
-	//});
+    //add an event listner to the arrow table element
+    $(".fa-search").on("click", triggerRecord);
+	
 //hide Record id in Virtual table
 $(document).on('knack-view-render.view_1169', function (event, view, data) {
 	  
