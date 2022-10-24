d9d9eb9b37c0160047f766e6a1758be9efd12866
Hynek <hynek@hruska.cz>
2022-10-24 12:17:36 +0200

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -3408,6 +3408,10 @@ function uploadVideoUploadStatusInKnack(message, recordId){
 
 $(document).on('knack-view-render.view_5612', function (event, view) {
   console.log('knack-view-render.view_5612z');
+  if ($('div[class*="field_8366"]>div').length===0){
+    console.log('NIC');
+    return;
+  }
   $('div[class*="field_8565"]').attr('id','infoText');
   if ($('div[class*="field_8366"] a[class="kn-view-asset"]').length===0){
     if ($('[id="infoText"]').text().trim()!==''){
