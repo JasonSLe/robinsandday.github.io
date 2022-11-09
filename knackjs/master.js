dbed8fa52522be50653a33323c347f7a1cc163cd
robinsandday <45364158+robinsandday@users.noreply.github.com>
2022-11-09 16:21:10 +0000

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -928,6 +928,14 @@ $(document).on('knack-scene-render.scene_1736', function(event, scene) {
   $(".kn-crumbtrail").hide();
 });
 
+$(document).on('knack-scene-render.scene_1737', function(event, scene) {
+  $(".kn-crumbtrail").hide();
+});
+
+$(document).on('knack-scene-render.scene_1739', function(event, scene) {
+  $(".kn-crumbtrail").hide();
+});
+
 // ----------  hide blank Enquiry Max table on New Vehicle P/X appraisal    ----------
 
 //$(document).on('knack-view-render.view_3254', function (event, view, data) {
