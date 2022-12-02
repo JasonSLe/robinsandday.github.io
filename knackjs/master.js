53b6d51d74de1e0bfc29e73f710844383efadeb6
Hynek <hynek@hruska.cz>
2022-12-02 18:05:00 +0100

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -429,7 +429,7 @@ $(document).on('knack-scene-render.any', function(event, scene) {
       });
     });
 
-    //menu work
+    //Not selecting the menu headers
     $('li[class="kn-dropdown-menu"]').each(function(){$(this).bind("click", function() {
         $('li[class*="is-active"]').removeClass('is-active');
       });
