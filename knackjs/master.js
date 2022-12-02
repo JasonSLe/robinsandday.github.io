e67fb63c35b7375ef4cfb963979e0448bae00521
Hynek <hynek@hruska.cz>
2022-12-02 18:06:25 +0100

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -434,13 +434,6 @@ $(document).on('knack-scene-render.any', function(event, scene) {
       });
     });
 
-    /*$('li[class="kn-dropdown-menu"]:has(a[class="kn-root-dropdown"]:not([href]))').each(function(){
-      $(this).bind("click", function() {
-        console.log('show menuV2');
-        $("#kn-mobile-menu").addClass('is-visible');
-      });
-    });*/
-
     var userBar = document.querySelector('.kn-info-bar .kn-current_user');
     toggleButton = document.createElement('span');
     toggleButton.classList.add('rad-toggle-header');
