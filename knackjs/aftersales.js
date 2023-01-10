6b9ecce0d6186a48ee2d952857d2b350cd1938f0
Hynek <hynek@hruska.cz>
2023-01-10 15:29:24 +0100

    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -1657,6 +1657,37 @@ $(document).on('knack-view-render.view_1297', function (event, view, data) {
   document.querySelector('div[class*="field_1687"]').appendChild(sound);
 })
 
+function createCookie(name, value, days) {
+  var expires;
+
+  if (days) {
+      var date = new Date();
+      date.setDate(date.getDate() + days); 
+      date.setHours(0);
+      expires = "; expires=" + date.toGMTString();
+  } else {
+      expires = "";
+  }
+  document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = encodeURIComponent(name) + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ')
+          c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0)
+          return decodeURIComponent(c.substring(nameEQ.length, c.length));
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  createCookie(name, "", -1);
+}
+
 $(document).on('knack-scene-render.any', function(event, scene) {
    //**************************************************************************************************************
 //****** Hynek's Code to check version on user Browser with what is stored in Apify. If version is different, 
