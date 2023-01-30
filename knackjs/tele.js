1bcb622a640b2287d0f5b3086b00fead5e23c76d
Hynek <hynek@hruska.cz>
2023-01-30 22:00:06 +0100

    dev

--- knackjs/tele.js
+++ knackjs/tele.js
@@ -774,3 +774,7 @@ var OperatingSystem = {
   }
   
   //end of shared camera app code
+
+  $(document).on('knack-view-render.view_4', function(event, view, data) {
+	prepareCameraView(location.origin+"/digital#used-vehicle-check-in/used-vehicle-check-in-2/"+getRecordIdFromHref(location.href)+"/used-vehicle-check-in-3/"+getRecordIdFromHref(location.href)+"/","591eae59e0d2123f23235769",'field_4944','scene_1543/views/view_5014');
+});
