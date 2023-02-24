450d763ff13e079c474d4a628782cac7638b570a
Hynek <hynek@hruska.cz>
2023-02-24 20:12:16 +0100

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -3719,7 +3719,7 @@ $(document).on('knack-view-render.view_2283', function (event, view, data) {
         url: 'https://7rhnwcwqj9ap.runs.apify.net/sightengine',
         type: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify({imageUrl:$('div[class="field_4944_thumb_100"] img').attr('data-kn-img-gallery'),recordId:getRecordIdFromHref(location.href)}),
+        data: JSON.stringify({imageUrl:$('div[class="field_4944_thumb_100"] img').attr('data-kn-img-gallery')}),
       }).then(function(resp) {
         let jsR = JSON.parse(resp);
         if (jsR.status==='success'){
@@ -3743,7 +3743,7 @@ $(document).on('knack-view-render.view_2283', function (event, view, data) {
         url: 'https://7rhnwcwqj9ap.runs.apify.net/detectron2',
         type: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify({imageUrl:$('div[class="field_4944_thumb_100"] img').attr('data-kn-img-gallery'),recordId:getRecordIdFromHref(location.href)}),
+        data: JSON.stringify({imageUrl:$('div[class="field_4944_thumb_100"] img').attr('data-kn-img-gallery')}),
       }).then(function(resp) {
         console.log('detecton2 resp', new Date());
         console.log(resp);
