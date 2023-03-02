c6f5986503de18c7ce52232c541024ef141a4ee4
DanielCheng1 <118259318+DanielCheng1@users.noreply.github.com>
2023-03-02 09:18:30 +0000

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -1264,6 +1264,10 @@ $(document).on('knack-form-submit.view_781', function(event, view, data) {
   callPostHttpRequest("https://hook.integromat.com/gmtkedwe7nxktiqm6qi4rg5apeno73an", {"Record ID":data.id},"Send Pre Visit Digital Customer Incident Form")
 });
 
+$(document).on('knack-form-submit.view_781', function(event, view, data) {
+  callPostHttpRequest("https://hook.eu1.make.celonis.com/fbuumn73d29ycs7o5ell2c4kflbnkhfb", {"Record ID":data.id},"Send Pre Visit Digital Customer Incident Form")
+});
+
 $(document).on('knack-form-submit.view_1394', function(event, view, data) {
   callPostHttpRequest("https://hook.eu1.make.celonis.com/e681sgmbzwk1hgugd3ph4kr34addh61o", {"Record ID":data.id,"Origin":data.field_1815},"Pre Visit Digital Customer Incident Form DEV")
 });
