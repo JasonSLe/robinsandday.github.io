76bb983d811313e3b8d973bb28c0ea400e88d541
Hynek <hynek@hruska.cz>
2023-03-28 10:42:42 +0200

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -3881,6 +3881,18 @@ function createPhotoRejectedButton(){
 }
 
 $(document).on('knack-view-render.view_2283', function (event, view, data) {
+  console.log('image',$('div[class*="field_4944_thumb_100"] img').attr('data-kn-img-gallery'));
+  if ($('div[class*="field_4944_thumb_100"] img').attr('data-kn-img-gallery')){
+    $.ajax({
+      url: 'https://7rhnwcwqj9ap.runs.apify.net/photoCheck',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({imageUrl:$('div[class="field_4944_thumb_100"] img').attr('data-kn-img-gallery'),recordId:$('input[name="id"]').attr('value')}),
+    })
+  }
+});
+
+$(document).on('knack-view-render.view_2283', function (event, view, data) {
   console.log('image',$('div[class="field_4944_thumb_100"] img').attr('data-kn-img-gallery'));
   if ($('div[class="field_4944_thumb_100"] img').attr('data-kn-img-gallery')){
     try{
