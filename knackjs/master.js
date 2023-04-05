69b04f60d98995c08ebbc3b242d3725628f8f5e9
Hynek <hynek@hruska.cz>
2023-04-05 23:10:38 +0200

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -3949,9 +3949,10 @@ function scene_1908_showhide(){
         sendRejectedPhotoToEnhancement(getRecordIdFromHref(location.href), $('div[data-input-id="field_7437"] img').attr('src'));
         window.setTimeout(function(){
           scene_1908_showhide();
+          keepRefreshingViewUntil('6166', false, '7416','TO BE CHECKED',scene_1908_showhide)
         }, 5000);
       }
-      
+
     }
   }
   if (!$('div[class*="field_4944"] img').attr('src')){
