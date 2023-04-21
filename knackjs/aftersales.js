4b6179a6758b7925946f70a9436c26c6b3223c8d
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2023-04-21 11:16:25 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -2354,3 +2354,29 @@ $(document).on('knack-form-submit.view_341', function(event, view, data) {
       $('div[class="field_'+fieldId+'"]').show();
     }, 100);
   }
+
+//hover field for Service On-site Workshop control view "Parts ave, awaiting labour"
+  $(document).on('knack-view-render.view_1558', function (event, view, data) {
+    //This part is for tooltip of another field above field in list
+    //This part of code hides field_330 from the list and then adds it as mouse over to field 380
+    //It needs function "getFieldForRowID", also the field_330 NEEDS to be included in the list
+    //start
+    $('th[class="field_318"]').hide();
+    $('td[class*="field_318"]').hide();
+    $('th[class="field_1537"]').hide();
+    $('td[class*="field_1537"]').hide(); 
+	 $('th[class="field_1532"]').hide();
+    $('td[class*="field_1532"]').hide(); 
+	  
+    $('div[id="view_1558"] table>tbody>tr').each(function(){
+      console.log($(this));
+      $(this).find('td[data-field-key="field_763"]').attr('title',getTextFromHTML(getFieldForRowID('view_1558','field_318',$(this).attr('id'))));
+      $(this).find('td[data-field-key="field_763"]').addClass('title');
+	    
+      $(this).find('td[data-field-key="field_899"]').attr('title',getTextFromHTML(getFieldForRowID('view_1558','field_1537',$(this).attr('id'))));
+      $(this).find('td[data-field-key="field_899"]').addClass('title');
+	  
+	        $(this).find('td[data-field-key="field_1021"]').attr('title',getTextFromHTML(getFieldForRowID('view_11559','field_1532',$(this).attr('id'))));
+      $(this).find('td[data-field-key="field_1021"]').addClass('title');
+    });
+	}); 
