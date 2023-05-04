be66dc878ccf7b57f80864a0aa5a747da88cdb93
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2023-05-04 15:58:07 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -2302,6 +2302,8 @@ $(document).on('knack-form-submit.view_341', function(event, view, data) {
     $('td[class*="field_318"]').hide();
     $('th[class="field_1537"]').hide();
     $('td[class*="field_1537"]').hide(); 
+    $('th[class="field_1532"]').hide();
+    $('td[class*="field_1532"]').hide(); 
 }); 
 
   $(document).on('knack-view-render.view_1898', function (event, view, data) {
