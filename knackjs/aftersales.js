c3f81ca9b65d2b57b92f498e546ff78052ffa513
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2023-05-26 11:53:23 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -2915,9 +2915,9 @@ $(document).on('knack-view-render.view_2298', function (event, view, data) {
 }); 
 //Workshop Controller all in one table (onsite jobs)
 $(document).on('knack-view-render.view_2249', function (event, view, data) {
-    tooltipsTable('755','2249','field_2240','field_2220');
-    tooltipsTable('755','2249','field_1537','field_2212');
-	tooltipsTable('755','2249','field_1532','field_2213');
+    tooltipsTable('761','2249','field_2240','field_2220');
+    tooltipsTable('761','2249','field_1537','field_2212');
+	tooltipsTable('761','2249','field_1532','field_2213');
 	     $('th[class="field_2240"]').hide();
     $('td[class*="field_2240"]').hide();
     $('th[class="field_1537"]').hide();
