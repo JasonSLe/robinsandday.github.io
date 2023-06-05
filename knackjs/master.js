f3572335a994a10b999213789d829b1fe0f240a4
robinsandday <45364158+robinsandday@users.noreply.github.com>
2023-06-05 12:19:05 +0100

    Update master.js
    line 1161 - change to hide all crumbtrail rather than scene specific

--- knackjs/master.js
+++ knackjs/master.js
@@ -1158,21 +1158,25 @@ $(document).on('knack-scene-render.scene_1314', function (event, view, data) {
 	$('[class="kn-info kn-container"]').hide();
 });
 
-$(document).on('knack-scene-render.scene_1719', function(event, scene) {
-  $(".kn-crumbtrail").hide();
+$(document).on('knack-scene-render.scene_any', function(event, scene) {
+$(".kn-crumbtrail").hide();
 });
 
-$(document).on('knack-scene-render.scene_1736', function(event, scene) {
-  $(".kn-crumbtrail").hide();
-});
+// $(document).on('knack-scene-render.scene_1719', function(event, scene) {
+//  $(".kn-crumbtrail").hide();
+// });
 
-$(document).on('knack-scene-render.scene_1737', function(event, scene) {
-  $(".kn-crumbtrail").hide();
-});
+// $(document).on('knack-scene-render.scene_1736', function(event, scene) {
+//  $(".kn-crumbtrail").hide();
+// });
 
-$(document).on('knack-scene-render.scene_1739', function(event, scene) {
-  $(".kn-crumbtrail").hide();
-});
+// $(document).on('knack-scene-render.scene_1737', function(event, scene) {
+//  $(".kn-crumbtrail").hide();
+// });
+
+// $(document).on('knack-scene-render.scene_1739', function(event, scene) {
+//  $(".kn-crumbtrail").hide();
+// });
 
 // ----------  hide blank Enquiry Max table on New Vehicle P/X appraisal    ----------
 
