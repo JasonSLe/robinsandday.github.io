afb6cd41d4c1dd7c6aa89e33282dc05cf1c56f8d
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2023-06-22 16:07:24 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -1178,10 +1178,6 @@ $(document).on('knack-scene-render.scene_1736', function(event, scene) {
   $(".kn-crumbtrail").hide();
  });
 
-$(document).on('knack-scene-render.scene_1773', function (event, view, data) {
-	$('[class="kn-view.kn-back-link"]').hide();
-	$('[class="kn-info kn-container"]').hide();
-});
 // ----------  hide blank Enquiry Max table on New Vehicle P/X appraisal    ----------
 
 //$(document).on('knack-view-render.view_3254', function (event, view, data) {
