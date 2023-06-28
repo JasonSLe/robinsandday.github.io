fde2e8bc3b380f1aeb05f6cbf682d8263c8187c6
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2023-06-28 14:20:29 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -2882,7 +2882,7 @@ $(document).on('knack-view-render.view_2246', function (event, view, data) {
     tooltipsTable('761','2246','field_1532','field_2220');
 	tooltipsTable('761','2246','field_1537','field_2213');
 	tooltipsTable('761','2246','field_2298','field_2272');
-	tooltipsTable('761','2246','field_1118','field_2278','Bay No: ');
+	tooltipsTable('761','2246','field_2278','field_1118','Tag No: ');
 	
 	     $('th[class="field_2240"]').hide();
     $('td[class*="field_2240"]').hide();
@@ -2890,8 +2890,8 @@ $(document).on('knack-view-render.view_2246', function (event, view, data) {
     $('td[class*="field_1537"]').hide(); 
 	  $('th[class="field_1532"]').hide();
     $('td[class*="field_1532"]').hide(); 
-	 $('th[class="field_1118"]').hide();
-    $('td[class*="field_1118"]').hide(); 
+	 $('th[class="field_2278"]').hide();
+    $('td[class*="field_2278"]').hide(); 
 }); 
 
 /*Workshop Controller all in one table (OFFsite jobs)
