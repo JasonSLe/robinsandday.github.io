37efa1276d6919272d9a1b863df5fe34cc536762
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2023-07-11 11:52:10 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -2147,7 +2147,7 @@ $(document).on('knack-view-render.view_1512', function (event, view, data) {
 // trigger to Send Data When Vehicle Is Checked Out From Customer Satisfaction check
 
 $(document).on('knack-form-submit.view_307', function(event, view, data) {
-  callPostHttpRequest("https://hook.eu1.make.celonis.com/e8f4buzy7rhplrdf1rgmclqkudy2mcno", {"Record ID":data.field_2223, "WIP":data.field_719, "POS":data.field_720, "Source": "View_307 - Satisfaction check(tablet)"},"Aftersales - customer satisfaction exit survey to trigger bot autoline check out")
+  callPostHttpRequest("https://hook.eu1.make.celonis.com/e8f4buzy7rhplrdf1rgmclqkudy2mcno", {"Record ID":data.field_2223, "WIP":data.field_719, "DemoTransactionNumber":data.field_1332, "POS":data.field_720, "Source": "View_307 - Satisfaction check(tablet)"},"Aftersales - customer satisfaction exit survey to trigger bot autoline check out")
 });
 
 // trigger to Send Data When Vehicle Is Checked Out From Customer Satisfaction check (sms)
