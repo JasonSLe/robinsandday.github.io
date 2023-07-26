08e2491ffa3aa9b4ef49e6b3992131887be2f6bf
DanielCheng1 <118259318+DanielCheng1@users.noreply.github.com>
2023-07-26 13:03:56 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -3259,6 +3259,14 @@ $(document).on('knack-form-submit.view_2940', function(event, view, data) {
   callPostHttpRequest("https://hook.eu1.make.celonis.com/ouosl7cqftin4d5xk4ybco0q96t5bwk2", {"Record ID":data.id,"Courtesy Car Agreement Record ID":data.field_2318_raw,"Forename":data.field_2461,"Surname":data.field_2462,"Date Of Birth":data.field_2325,"First Line Of Address":data.field_2312,"Postcode":data.field_2314,"Email Address":data.field_2315_raw,"Driving Licence Number":data.field_2316},"Customer and Driver NOT Same Person + Manually Entering Driving Licence Details")
 });
 
+$(document).on('knack-form-submit.view_3001', function(event, view, data) {
+  callPostHttpRequest("https://hook.eu1.make.celonis.com/ouosl7cqftin4d5xk4ybco0q96t5bwk2", {"Record ID":data.id,"Driving Licence Check Record ID":data.field_2318_raw,"Date Of Birth":data.field_2325,"Driving Licence Number":data.field_2316},"Customer and Driver Same Person + Manually Entering Driving Licence Details")
+});
+
+$(document).on('knack-form-submit.view_3002', function(event, view, data) {
+  callPostHttpRequest("https://hook.eu1.make.celonis.com/ouosl7cqftin4d5xk4ybco0q96t5bwk2", {"Record ID":data.id,"Driving Licence Check Record ID":data.field_2318_raw,"Forename":data.field_2461,"Surname":data.field_2462,"Date Of Birth":data.field_2325,"First Line Of Address":data.field_2312,"Postcode":data.field_2314,"Email Address":data.field_2315_raw,"Driving Licence Number":data.field_2316},"Customer and Driver NOT Same Person + Manually Entering Driving Licence Details")
+});
+
 //trigger bot for exit survey (job card v2 - Check out via tablet)
 $(document).on('knack-form-submit.view_2364', function(event, view, data) {
   callPostHttpRequest("https://hook.eu1.make.celonis.com/e8f4buzy7rhplrdf1rgmclqkudy2mcno", {"Record ID":data.field_2223, "WIP":data.field_719, "DemoTransactionNumber":data.field_2476, "POS":data.field_720, "Source": "View_2364 - Satisfaction check(tablet)"},"Aftersales - customer satisfaction exit survey to trigger bot autoline check out")
