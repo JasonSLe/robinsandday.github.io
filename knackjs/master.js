6fe067b302e3dab6289ee5e8baf777c4a614c39a
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2023-10-17 14:36:53 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -404,6 +404,12 @@ $(document).on('knack-view-render.view_6388', function(event, view, data) {
   $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/aftersales#activity-snapshot/?token='+encodeURIComponent(token) + '" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
 });
 
+//Stock Search
+$(document).on('knack-view-render.view_6445', function(event, view, data) {
+  var token = Knack.getUserAttributes().values["field_6440"];
+  $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/digital-orders?token='+encodeURIComponent(token) + '#cronos-stock-search/" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
+});
+
 
 var aftersalesConnectView = [{view:'view_6320',url:'#technician-view-my-jobs-v2'},
 {view:'view_5733',url:'#after-sales-vehicle-lookup/'},
