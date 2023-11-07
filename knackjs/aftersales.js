188b9e4ce88714224ef0a22d7e40f075f123e5a0
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2023-11-07 11:21:30 +0000

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -529,7 +529,7 @@ function refreshScene24(){
     },{	    
       name : 'Recalls',
       mainField : 'field_70', //Recalls and service shedule check Completed
-      views:['329','332']
+      views:['329','332','3276']
     }
   ]
   sceneRefresh(refreshData);
