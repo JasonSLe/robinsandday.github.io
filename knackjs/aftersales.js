3c1ddd3d7a168cdbbcab4fca063587348553e38e
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2023-11-07 12:24:02 +0000

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -3723,5 +3723,5 @@ $(document).on('knack-view-render.view_3278', function (event, view, data) {
     showHideMoreServiceVisits();
   }
   $('div[class="field_325"]').hide();
-  serviceVisitsTooltips('3278','325');
+  tooltipsTable('1044','3278','field_325','field_309');
 });
