6c19715f370231c92e49459b10ab68285656768e
robinsandday <45364158+robinsandday@users.noreply.github.com>
2023-11-08 12:40:23 +0000

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -3726,6 +3726,7 @@ $(document).on('knack-view-render.view_3278', function (event, view, data) {
   serviceVisitsTooltips('3278','325','tooltipTop');
 });
 
+//Prevents clicking off a Popup Modal
 $(document).on(‘knack-scene-render.scene_769’, function(event, scene) {
 $(’.kn-modal-bg’).off(‘click’);
 });
