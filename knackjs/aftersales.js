4d5238679f400778bc60e37b08d29fd99c573451
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2023-11-22 15:56:35 +0000

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -3801,8 +3801,20 @@ $(document).on('knack-view-render.view_3278', function (event, view, data) {
     $('td[class*="field_1537"]').hide();
    	
 	tooltipsTable('1098','3474','field_1537','field_2213');  
-//    $('div[id="view_3474"] table>tbody>tr').each(function(){
-  //    $(this).find('td[data-field-key="field_2213"]').attr('data-tooltip',getFieldForRowID('view_3474','field_1537',$(this).attr('id')));
-    //  $(this).find('td[data-field-key="field_2213"]').addClass('tooltip-top');
 	    
     });
+
+	//hover for labour details on workshop POT (service Table)
+   $(document).on('knack-view-render.view_3476', function (event, view, data) {
+    //This part is for tooltip of another field above field in list
+    //This part of code hides field_330 from the list and then adds it as mouse over to field 380
+    //It needs function "getFieldForRowID", also the field_330 NEEDS to be included in the list
+    //start
+    $('th[class="field_1537"]').hide();
+    $('td[class*="field_1537"]').hide();
+   	
+	tooltipsTable('1098','3476','field_1537','field_2213');  
+
+	    
+    });
+	
