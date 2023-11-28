5debac6696069093a05c244ff7061854e5cee7bc
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2023-11-28 10:50:35 +0000

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -939,6 +939,28 @@ $(document).on('knack-form-submit.view_1474', function(event, view, data) {
     }
 });
 
+  //trigger get tyres and prices from pre-visit jobcard
+$(document).on('knack-form-submit.view_3515', function(event, view, data) { 
+    
+    try{
+        
+
+        let commandURL = "https://hook.eu1.make.celonis.com/f3xcida5tqk6fybgpkga8p9gn7ek6e7o";
+        let dataToSend = JSON.stringify({"Record ID":data.id, "REG":data.field_31, "POS":data.field_443, "Dealer":data.field_411});
+
+        var rData = $.ajax({
+            url: commandURL,
+            type: 'POST',
+            contentType: 'application/json',
+            data: dataToSend,
+            async: false
+        }).responseText;
+    }catch(exception){
+        sendErrorToIntegromat(exception, "Trigger get tyres and prices from customer job card");
+    }
+});
+  
+
 /*trigger get tyres and prices for a selected dealer
 $(document).on('knack-form-submit.view_1474', function(event, view, data) { 
     
