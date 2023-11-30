848182411a8872054fdc8adecd010abe611e0f33
Hynek <hynek@hruska.cz>
2023-11-30 07:06:24 +0100

    dev

--- knackjs/takePhotoApp.js
+++ knackjs/takePhotoApp.js
@@ -317,14 +317,16 @@ const effect = $('#cameraOverlayCanvas');
 var imageBeforeResize = document.createElement('img');
 imageBeforeResize.onload = () => {
   alert(imageBeforeResize.width+'*'+imageBeforeResize.height)
+  let imageRatio = imageBeforeResize.width/imageBeforeResize.height;
+
    const elem = document.createElement('canvas');
-   elem.width = (appSettings.resizeImageMaxWidth?appSettings.resizeImageMaxWidth: imageBeforeResize.width);
-   elem.height = (appSettings.resizeImageMaxHeight?appSettings.resizeImageMaxHeight: imageBeforeResize.height);
+   elem.width = (appSettings.resizeImageMaxWidth?(imageRatio>=1?appSettings.resizeImageMaxWidth:appSettings.resizeImageMaxWidth*imageRatio): imageBeforeResize.width);
+   elem.height = (appSettings.resizeImageMaxHeight?(imageRatio>=1?appSettings.resizeImageMaxHeight*(1/imageRatio):appSettings.resizeImageMaxHeight): imageBeforeResize.height);
    const ctx = elem.getContext('2d');
   //check if the resolution of the image is 4:3
 
  //ONE STEP RESIZE
-    ctx.drawImage(imageBeforeResize,0,0,imageBeforeResize.width,imageBeforeResize.height);//, imageBeforeResize.width * (1-percentOfPicture)/2, imageBeforeResize.height * (1-percentOfPicture)/2, imageBeforeResize.width * percentOfPicture,imageBeforeResize.height * percentOfPicture, 0, 0, 768, 576);
+    ctx.drawImage(imageBeforeResize,0,0,elem.width,elem.height);//, imageBeforeResize.width * (1-percentOfPicture)/2, imageBeforeResize.height * (1-percentOfPicture)/2, imageBeforeResize.width * percentOfPicture,imageBeforeResize.height * percentOfPicture, 0, 0, 768, 576);
   
    //save the resized image to the shown img
    ctx.canvas.toBlob((blob) => {
