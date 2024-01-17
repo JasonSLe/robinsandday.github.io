634be12739e5d629ea34a73af1ea912bff05ea43
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2024-01-17 13:52:23 +0000

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -4426,3 +4426,12 @@ function scanDocsLinkFunction(selector_view){
     }
 
   });
+
+//auto refresh for workshop controller pots
+function recursivecallscene_1098(){
+ setTimeout(function () { if($("#view_3474").is(":visible")==true){ Knack.views["view_3474"].model.fetch();recursivecallscene_1098();} }, 30000);
+ setTimeout(function () { if($("#view_3476").is(":visible")==true){ Knack.views["view_3476"].model.fetch();recursivecallscene_1098();} }, 30000);
+ setTimeout(function () { if($("#view_3483").is(":visible")==true){ Knack.views["view_3483"].model.fetch();recursivecallscene_1098();} }, 30000);
+ setTimeout(function () { if($("#view_3482").is(":visible")==true){ Knack.views["view_3482"].model.fetch();recursivecallscene_1098();} }, 30000);
+ setTimeout(function () { if($("#view_3477").is(":visible")==true){ Knack.views["view_3477"].model.fetch();recursivecallscene_1098();} }, 30000);
+}
